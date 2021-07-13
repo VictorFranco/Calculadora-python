7f609b54cbadb7c59d5a2cf21eafafeed0ada00d
Victor Franco <vfrancohdez1@hotmail.com>
2021-07-13 12:00:38 -0500

    Mejorar input de calculadora
    Cambiar los colores de background y foreground de entry para mejorar
    contraste.

--- Calculadora.py
+++ Calculadora.py
@@ -11,12 +11,21 @@ app_posx=int(screen_width/2-app_width/2)
 app_posy=int(screen_height/2-app_height/2)
 window.geometry(f"{app_width}x{app_height}+{app_posx}+{app_posy}")# dimensiones de ventana
 
+entry_color="#B8C9F5"
 calc_color="#141E36"
 btn_color="#5D88F5"
 btn_group_color="#2E437A"
 
 window.configure(background=calc_color)
-txt=Entry(window,text="Escribe un numero",font=("Arial Bold", 15),width=15,state='disabled')
+
+txt=Entry(window,
+        disabledbackground=entry_color,
+        disabledforeground="black",
+        font=("Arial Bold", 15),
+        width=15,
+        state='disabled',
+        borderwidth=0,highlightthickness=0)
+
 txt.grid(columnspan=3,row=0,pady=10)
 btn=[[0]*4]*4
 frame=LabelFrame(window,background=btn_group_color,borderwidth=0,highlightthickness=0)
@@ -47,6 +56,10 @@ for x in range(0,4):
                 tecla="."
             if(x==2):
                 tecla="="
-        btn[x][y]=Button(frame,background=btn_color,borderwidth=0,highlightthickness=0,text=tecla,command=lambda tecla=tecla: Funciones.clicked(tecla,txt),width=2,height=1, font=("Arial Bold", 20))
+        btn[x][y]=Button(frame,background=btn_color,
+                text=tecla,
+                command=lambda tecla=tecla: Funciones.clicked(tecla,txt),
+                width=2,height=1, font=("Arial Bold", 20),
+                borderwidth=0,highlightthickness=0)
         btn[x][y].grid(column=x,row=y,pady=6,padx=6)
 window.mainloop()
