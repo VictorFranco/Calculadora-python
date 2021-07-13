39cbdd96481aac625d7fa9083e78077e68c8c1fd
Victor Franco <vfrancohdez1@hotmail.com>
2021-07-13 11:30:09 -0500

    Mejorar estilo
    Cambiar los colores base y los bordes de los botones.

--- Calculadora.py
+++ Calculadora.py
@@ -3,7 +3,7 @@ import Funciones
 window=Tk()
 window.title("Calculadora")
 
-app_width=280
+app_width=293
 app_height=300
 screen_width=window.winfo_screenwidth()
 screen_height=window.winfo_screenheight()
@@ -11,11 +11,15 @@ app_posx=int(screen_width/2-app_width/2)
 app_posy=int(screen_height/2-app_height/2)
 window.geometry(f"{app_width}x{app_height}+{app_posx}+{app_posy}")# dimensiones de ventana
 
-window.configure(background="gray")
+calc_color="#141E36"
+btn_color="#5D88F5"
+btn_group_color="#2E437A"
+
+window.configure(background=calc_color)
 txt=Entry(window,text="Escribe un numero",font=("Arial Bold", 15),width=15,state='disabled')
 txt.grid(columnspan=3,row=0,pady=10)
 btn=[[0]*4]*4
-frame=LabelFrame(window)
+frame=LabelFrame(window,background=btn_group_color,borderwidth=0,highlightthickness=0)
 frame.grid(column=1,row=1,pady=10,padx=6)
 for x in range(0,4):
     for y in range(0,4):
@@ -43,6 +47,6 @@ for x in range(0,4):
                 tecla="."
             if(x==2):
                 tecla="="
-        btn[x][y]=Button(frame,text=tecla,command=lambda tecla=tecla: Funciones.clicked(tecla,txt),width=2,height=1, font=("Arial Bold", 20))
-        btn[x][y].grid(column=x,row=y,pady=2,padx=2)
+        btn[x][y]=Button(frame,background=btn_color,borderwidth=0,highlightthickness=0,text=tecla,command=lambda tecla=tecla: Funciones.clicked(tecla,txt),width=2,height=1, font=("Arial Bold", 20))
+        btn[x][y].grid(column=x,row=y,pady=6,padx=6)
 window.mainloop()
